954d7fa329ca49f7b8701af9b7a33c4bae151d57
Yogi <qq643374843@163.com>
2022-11-06 22:50:53 +0800

    try

--- CurrentTab/MainWindow.xaml.cs
+++ CurrentTab/MainWindow.xaml.cs
@@ -1,18 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
 namespace CurrentTab
 {
     /// <summary>
@@ -23,6 +11,26 @@ namespace CurrentTab
         public MainWindow( )
         {
             InitializeComponent ();
+        }
+        //A test code by yogi at 2022.10.23
+        public virtual void MyMethod( )
+        {
+            Console.WriteLine ("print whathappend?");
+            throw new NotImplementedException ();
+        }
+
+        private void Button_Click( object sender , RoutedEventArgs e )
+        {
+
+        }
+
+        private void UIElement_OnMouseLeftButtonDown( object sender , System.Windows.Input.MouseButtonEventArgs e )
+        {
+
+            if( e.ButtonState == MouseButtonState.Pressed )
+            {
+                DragMove ();
+            }
         }
     }
 }
